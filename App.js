da49d3c9f97b035282629084d3c1d96cc92dafe4
Reshmi Ranjith <rreshmiranjith@gmail.com>
2019-07-29 11:30:25 -0500

    use a AnimatedFlatList

--- App.js
+++ App.js
@@ -1,40 +1,60 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import { View, FlatList, Text, StyleSheet, Animated } from 'react-native';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, ScrollView} from 'react-native';
+const data = ['Hello', 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 19, 20, 21, 22, 23, 24, 25, 26];
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
 
-export default class App extends Component {
-  render() {
-    return (
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <Text style={styles.welcome}>Hello!</Text>
-        <Text style={styles.welcome}>Welcome to React Native</Text>
-        <Text style={styles.welcome}>Welcome to React Native</Text>
-        <Text style={styles.welcome}>Welcome to React Native</Text>
-        <Text style={styles.welcome}>Welcome to React Native</Text>
-        <Text style={styles.welcome}>Welcome to React Native</Text>
-        <Text style={styles.welcome}>Welcome to React Native</Text>
-        <Text style={styles.welcome}>Welcome to React Native</Text>
-        <Text style={styles.welcome}>Welcome to React Native</Text>
-      </ScrollView>
-    );
-  }
+export default class AnimatedHeader extends React.Component {
+	state = {
+		animatedValue: new Animated.Value(0),
+	};
+	
+	_renderItem = ({item}) => {
+		return (
+			<View style={styles.welcome}>
+				<Text style={styles}>{item}</Text>
+			</View>
+		)
+	};
+	
+	render() {
+		let translateY = this.state.animatedValue.interpolate({
+			inputRange: [0, 180],
+			outputRange: [0, -180],
+			extrapolate: 'clamp',
+		});
+		
+		return (
+			<View style={styles.container}>
+				<AnimatedFlatList
+					contentContainerStyle={{marginTop: 200}}
+					scrollEventThrottle={16} // <-- Use 1 here to make sure no events are ever missed
+					onScroll={Animated.event(
+						[{nativeEvent: {contentOffset: {y: this.state.animatedValue}}}],
+						{useNativeDriver: true} // <-- Add this
+					)}
+					data={data}
+					renderItem={this._renderItem}
+					keyExtractor={(item, i) => i}/>
+				<Animated.View style={[styles.headerWrapper, {transform: [{translateY}]}]}/>
+			</View>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: 'lightblue',
+	},
+	headerWrapper: {
+		position: 'absolute',
+		backgroundColor: 'red',
+		height: 200,
+		left: 0,
+		right: 0,
+  },
   welcome: {
     flex: 1,
     margin: 20,
@@ -42,6 +62,6 @@ const styles = StyleSheet.create({
     margin: 10,
     textAlign: 'center',
     fontSize: 20,
-    padding: 70,
+    padding: 20,
   }
 });
