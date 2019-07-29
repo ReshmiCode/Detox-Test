d70f5b494e9ec04bf359cb2c201db057426ba362
Reshmi Ranjith <rreshmiranjith@gmail.com>
2019-07-29 11:17:29 -0500

    add stuff to view

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, ScrollView} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -19,30 +19,29 @@ const instructions = Platform.select({
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Text style={styles.welcome}>Hello!</Text>
+        <Text style={styles.welcome}>Welcome to React Native</Text>
+        <Text style={styles.welcome}>Welcome to React Native</Text>
+        <Text style={styles.welcome}>Welcome to React Native</Text>
+        <Text style={styles.welcome}>Welcome to React Native</Text>
+        <Text style={styles.welcome}>Welcome to React Native</Text>
+        <Text style={styles.welcome}>Welcome to React Native</Text>
+        <Text style={styles.welcome}>Welcome to React Native</Text>
+        <Text style={styles.welcome}>Welcome to React Native</Text>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
   welcome: {
-    fontSize: 20,
-    textAlign: 'center',
+    flex: 1,
+    margin: 20,
+    backgroundColor: 'orange',
     margin: 10,
-  },
-  instructions: {
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    fontSize: 20,
+    padding: 70,
+  }
 });
