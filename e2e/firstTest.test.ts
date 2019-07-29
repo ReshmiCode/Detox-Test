13fe6539ef3064aa5e7ec13f3d4fcee5a60e0333
Reshmi Ranjith <rreshmiranjith@gmail.com>
2019-07-29 11:17:45 -0500

    switch test to ts

--- e2e/firstTest.test.ts
+++ e2e/firstTest.test.ts
@@ -1,9 +1,12 @@
+import { expect } from 'detox';
+
 describe('Example', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
   });
 
   it('should have welcome screen', async () => {
-    await expect(element(by.text('Welcome!'))).toExist();
+    await expect(element(by.text('Hello!'))).toExist();
+    await element(by.text('Hello!')).swipe('up', 'fast', 0.89);
   });
 });
