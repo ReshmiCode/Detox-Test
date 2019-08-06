00f085a412c95916c8421c32ea98f942d4a8ea29
Reshmi Ranjith <rreshmiranjith@gmail.com>
2019-08-06 16:56:48 -0500

    add basic unit test

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,14 +1,9 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
 import App from '../App';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import { render } from 'react-native-testing-library';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  const screen = render(<App />);
+  expect(screen.queryAllByText("Hello!")).not.toBeNull();
 });
