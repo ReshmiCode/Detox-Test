c1106c0682cf26ca5623f12395e2a026668545bf
Reshmi Ranjith <rreshmiranjith@gmail.com>
2019-08-06 17:08:06 -0500

    get unit test retrying

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -3,7 +3,9 @@ import React from 'react';
 import App from '../App';
 import { render } from 'react-native-testing-library';
 
+jest.retryTimes(2);
 it('renders correctly', () => {
+  console.log('trying');
   const screen = render(<App />);
-  expect(screen.queryAllByText("Hello!")).not.toBeNull();
+  expect(screen.queryByText("Hello!z")).not.toBeNull();
 });
