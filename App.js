09df3c8ebc1cc6a6b0b0f8716ac438ada3d7da31
Reshmi Ranjith <rreshmiranjith@gmail.com>
2019-08-12 17:10:59 -0500

    show splash screen for a little bit

--- App.js
+++ App.js
@@ -4,9 +4,7 @@ import {StyleSheet, Button, Text, View} from 'react-native';
 
 export default class App extends Component {
 	componentDidMount() {
-    	// do stuff while splash screen is shown
-        // After having done stuff (such as async tasks) hide the splash screen
-        SplashScreen.hide();
+		setTimeout(function() { SplashScreen.hide() }, 300);
 	}
 	
 	render() {
