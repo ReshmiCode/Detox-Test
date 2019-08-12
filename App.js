fd652c057cd2ce111810fa6defaf5530b62aa427
Reshmi Ranjith <rreshmiranjith@gmail.com>
2019-08-12 17:05:31 -0500

    hide splash screen after

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
+import SplashScreen from 'react-native-splash-screen'
 import {StyleSheet, Button, Text, View} from 'react-native';
 
 export default class App extends Component {
+	componentDidMount() {
+    	// do stuff while splash screen is shown
+        // After having done stuff (such as async tasks) hide the splash screen
+        SplashScreen.hide();
+	}
+	
 	render() {
 	  return (
 		<View style={styles.container}>
